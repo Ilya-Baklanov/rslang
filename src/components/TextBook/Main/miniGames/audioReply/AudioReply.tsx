5cc1b3d8f86b3c87cb274419942abeeecc2f9b44
Vitaly Kniazev <vitaly_kniazev@epam.com>
2021-04-14 22:12:53 +0300

    feat: add Audio Reply Game

--- src/components/TextBook/Main/miniGames/audioReply/AudioReply.tsx
+++ src/components/TextBook/Main/miniGames/audioReply/AudioReply.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Button } from 'react-bootstrap';
+import GameResults from '@/types/gameresult.types';
+import { AggregatedWord, AggregatedWords } from '@/types/response.types';
+import getAggregatedWords from '@/utils/getAggregatedWords';
+
 import GameContainer from '../gamesTools/gameContainer/GameContainer';
 import GameCounter from '../gamesTools/gameCounter/GameCounter';
-import HealthIndicator from '../gamesTools/healthIndicator/HealthIndicator';
+import InfoIcon from '../gamesTools/infoIcon/infoIcon';
 import MicIcon from '../gamesTools/micIcon/MicIcon';
+import StatModal from '../gamesTools/statModal/StatModal';
 import WordPlate from '../gamesTools/wordPlate/WordPlate';
 
 import aReplyStyles from './AudioReply.scss';
@@ -12,10 +16,101 @@ import aReplyStyles from './AudioReply.scss';
 const invitePressAndSpeak = 'Нажмите кнопку микрофона и произнесите слово';
 const inviteSpeak = 'Произнесите слово';
 const resultDisplayDelay = 1000;
+const infoIconDisplayDelay = 2000;
+
+//* ****LEVEL PARAMS*****
+const group = 1;
+const page = 1;
+const wordsQty = 20;
+//* *********************
 
 function AudioReply(): JSX.Element {
   const [recording, setRecording] = useState<boolean>(false);
   const [invitation, setInvitation] = useState<string>(invitePressAndSpeak);
+
+  const [words, setWords] = useState<number>(wordsQty);
+  const [isAnswerShow, setIsAnswerShow] = useState(false);
+  const [isDisplayInfo, setIsDisplayInfo] = useState(false);
+  const [isRightAnswerReceived, setIsRightAnswerReceived] = useState(true);
+  const [gameResults, setGameResults] = useState<GameResults>({ goodAnswers: [], badAnswers: [] });
+  const [russianWord, setRussianWord] = useState<string>('');
+  const [currentWord, setCurrentWord] = useState(0);
+  const [wordsList, setWordList] = useState<AggregatedWord[]>([]);
+  const [showResults, setShowResults] = useState(false);
+
+  function exitGame() {
+    setShowResults(false);
+    // Здесь необходимо подключить переход на страницу с играми!!!!!
+    // Результат игры в gameResults
+  }
+
+  function proceedWithWord(wordNumber: number) {
+    if (wordNumber < wordsList.length) {
+      setRussianWord(wordsList[currentWord].wordTranslate);
+    } else {
+      setShowResults(true);
+    }
+  }
+
+  function showInfo() {
+    setIsDisplayInfo(true);
+    setIsAnswerShow(true);
+    setTimeout(() => {
+      setIsDisplayInfo(false);
+      setIsAnswerShow(false);
+      setWords((prev: number) => prev - 1);
+      setCurrentWord((prev: number) => prev + 1);
+    }, infoIconDisplayDelay);
+  }
+
+  function processResult(isCorrectAnswer: boolean): void {
+    const answerRecord = wordsList[currentWord];
+    if (isCorrectAnswer) {
+      setGameResults((prev: GameResults) => {
+        prev.goodAnswers.push(answerRecord);
+        return prev;
+      });
+    } else {
+      setGameResults((prev: GameResults) => {
+        prev.badAnswers.push(answerRecord);
+        return prev;
+      });
+    }
+  }
+
+  useEffect(() => {
+    let cleanupFunction = false;
+    getAggregatedWords(group, page, wordsQty, 'empty')
+      .then((content: AggregatedWords) => {
+        if (!cleanupFunction) setWordList(content.paginatedResults);
+      })
+      .catch(() => {});
+    return () => {
+      cleanupFunction = true;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (wordsList.length) {
+      proceedWithWord(currentWord);
+    }
+  }, [wordsList]);
+
+  useEffect(() => {
+    if (currentWord) proceedWithWord(currentWord);
+  }, [currentWord]);
+
+  function onGoodAnswer() {
+    setIsRightAnswerReceived(true);
+    processResult(true);
+    showInfo();
+  }
+
+  function onBadAnswer() {
+    setIsRightAnswerReceived(false);
+    processResult(false);
+    showInfo();
+  }
 
   function onRecordingClick() {
     if (!recording) {
@@ -36,8 +131,12 @@ function AudioReply(): JSX.Element {
 
         recognition.onresult = (event: SpeechRecognitionEvent) => {
           if (event.results.length > 0) {
-            setInvitation(event.results[0][0].transcript);
-          }
+            const result: string = event.results[0][0].transcript;
+            setInvitation(result);
+            if (result.toLowerCase() === wordsList[currentWord].word.toLowerCase()) {
+              onGoodAnswer();
+            } else onBadAnswer();
+          } else onBadAnswer();
         };
       }
     }
@@ -45,10 +144,16 @@ function AudioReply(): JSX.Element {
 
   function AudioReplyGame(): JSX.Element {
     return (
-      <div>
-        <GameCounter label="СЛОВА:" count={3} />
-        <HealthIndicator count={3} />
-        <WordPlate label="Слово" isBig />
+      <div className={aReplyStyles['audio-reply-game']}>
+        <GameCounter label="СЛОВА:" count={words} />
+        <WordPlate label={russianWord} isBig />
+        {isAnswerShow && (
+          <WordPlate
+            label={`${wordsList[currentWord].word} - ${wordsList[currentWord].transcription}`}
+          />
+        )}
+        {isDisplayInfo && <InfoIcon isPositive={isRightAnswerReceived} />}
+        <StatModal gameResults={gameResults} statShow={showResults} onHide={exitGame} />
       </div>
     );
   }
