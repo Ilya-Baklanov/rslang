9878d715b46138b9abbe60691be1e13942d03fce
d.ananyev <Dzmitry.Ananyeu@digital14.com>
2021-04-15 02:26:52 +0400

    add bg images and css

--- src/components/MainPage/Footer/Footer.tsx
+++ src/components/MainPage/Footer/Footer.tsx
@@ -5,11 +5,19 @@ import styles from './style.scss';
 const Footer = (): JSX.Element => (
   <div className={styles['footer']}>
     <div className={styles['developers']}>
-      <a href="https://github.com/Ilya-Baklanov">Ilya</a>
-      <a href="https://github.com/dimit999">Dmitry</a>
-      <a href="https://github.com/vitaly-kn">Vitaly</a>
+      <a href="https://github.com/Ilya-Baklanov" className={styles['oneDev']}>
+        Ilya
+      </a>
+      <a href="https://github.com/dimit999" className={styles['twoDev']}>
+        Dmitry
+      </a>
+      <a href="https://github.com/vitaly-kn" className={styles['threeDev']}>
+        Vitaly
+      </a>
     </div>
-    <div className={styles['year']}>2021</div>
+    <div className={styles['bubbles']}>
+      <h1 className={styles['year']}>2021</h1>
+    </div>
     <a href="https://rs.school/js/">
       <img
         src="/assets/image/rs_school_js.svg"
