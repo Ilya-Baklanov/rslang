144ffc56da47602ccac97dff8c8c482f5c6d3773
d.ananyev <Dzmitry.Ananyeu@digital14.com>
2021-04-15 12:40:48 +0400

    add images preview

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -8,33 +8,35 @@ import styles from './style.scss';
 
 const MainPage = (): JSX.Element => (
   <React.Fragment>
-    <div className={styles['main-page-wrapper']}>
-      <header className={styles['header-container']}>
-        <Header />
-      </header>
-      <main className={styles['main']}>
-        <div className={styles['preview']} id="preview">
-          <StartLearning />
-        </div>
-        <div className={styles['description']} id="description">
-          особенности
-        </div>
-        <div className={styles['demonstration-app']} id="demonstration-app">
-          демонстрация работы приложения
-        </div>
-        <div className={styles['about-the-team']} id="about-the-team">
-          о команде
-        </div>
-        <div className={styles['mini-games']} id="mini-games">
-          мини игры
-        </div>
-        <div className={styles['statistics']} id="statistics">
-          статистика
-        </div>
-      </main>
-      <footer className={styles['footer-container']}>
-        <Footer />
-      </footer>
+    <div className={styles['main-container-wrapper']}>
+      <div className={styles['main-page-wrapper']}>
+        <header className={styles['header-container']}>
+          <Header />
+        </header>
+        <main className={styles['main']}>
+          <div className={styles['preview']} id="preview">
+            <StartLearning />
+          </div>
+          <div className={styles['description']} id="description">
+            особенности
+          </div>
+          <div className={styles['demonstration-app']} id="demonstration-app">
+            демонстрация работы приложения
+          </div>
+          <div className={styles['about-the-team']} id="about-the-team">
+            о команде
+          </div>
+          <div className={styles['mini-games']} id="mini-games">
+            мини игры
+          </div>
+          <div className={styles['statistics']} id="statistics">
+            статистика
+          </div>
+        </main>
+        <footer className={styles['footer-container']}>
+          <Footer />
+        </footer>
+      </div>
     </div>
   </React.Fragment>
 );
